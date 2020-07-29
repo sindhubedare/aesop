b7c5142361b62b3a2c6b06db90cc7f060f8c5623
sindhubedare <sindhubedare1990@gmail.com>
2020-07-29 10:16:49 -0500

    introduction

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Images';
+import Page1 from './Component/Page1';
+
+function App() {
+  return (
+    <div className="App">
+      <Page1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Page1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Images';
+
+class Page1 extends React.Component{
+    render(){
+     return(
+         <div>
+             <img src= "./Images/aromatic_signature_products"/>
+         </div>
+     )
+    }
+}
+
+export default Page1;
